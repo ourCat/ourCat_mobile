8553f3526bfed4b245f392f41999983f4d5ad61d
Min Jae Kim <exploit017@gmail.com>
2021-11-22 19:33:46 +0900

    init porject

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ourCat_mobile
 ourCat_mobile
+
+lib
+
+  - models
+  
+  - providers
+
+  - screens
+
+  - utils
+
+  - widgets
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Our Cat', home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Our Cat'),
+      ),
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
